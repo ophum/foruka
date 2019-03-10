f495a71a680a0b398f2b9e041134189353846153
ophum <inagaki0106@gmail.com>
2019-03-10 22:34:02 +0900

    [update] decorated table

--- resources/views/containers/index.blade.php
+++ resources/views/containers/index.blade.php
@@ -1,14 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<ul>
-<a href="{{ route('containers.create') }}"><li>create container</li></a>
-</ul>
 
-<table class="table">
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>Container Name</th>
+            <th>Image Name</th>
+            <th>Status</th>
+        </tr>
+    </thead>
+
+    <tbody>
 @foreach($containers as $c)
-    <tr><td><a href="{{ route('containers.show', $c->id) }}">{{ $c->name }}</a></td><td>{{ $c->image->name }}</td></tr>
+        <tr>
+            <td>
+                <a href="{{ route('containers.show', $c->id) }}"><div>{{ $c->name }}</div></a>
+            </td>
+            <td>
+                {{ $c->image->name }}
+            </td>
+            <td>
+
+            </td>
+        </tr>
 @endforeach
+    </tbody>
 </table>
 
 @endsection
