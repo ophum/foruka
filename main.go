9fdc811a7a33c9e0445d33a67930dd2d2e120301
ophum <inagaki0106@gmail.com>
2019-04-12 14:57:29 +0900

    [add] add containerController

--- main.go
+++ main.go
@@ -1,32 +1,37 @@
 package main
 
-import(
-  "github.com/gin-gonic/gin"
-  "github.com/gin-gonic/contrib/sessions"
-  _ "net/http"
+import (
+	_ "net/http"
 
-  home "github.com/ophum/foruka/controllers/homeController"
-  auth "github.com/ophum/foruka/controllers/authController"
+	"github.com/gin-gonic/contrib/sessions"
+	"github.com/gin-gonic/gin"
+
+	auth "github.com/ophum/foruka/controllers/authController"
+	home "github.com/ophum/foruka/controllers/homeController"
 )
 
 func main() {
-  r := gin.Default()
+	r := gin.Default()
 
-  store := sessions.NewCookieStore([]byte("secret"))
-  r.Use(sessions.Sessions("session", store))
+	store := sessions.NewCookieStore([]byte("secret"))
+	r.Use(sessions.Sessions("session", store))
 
-  r.LoadHTMLGlob("views/templates/*")
+	r.LoadHTMLGlob("views/templates/*/**")
 
-  r.GET("/", home.Index)
+	r.GET("/", home.Index)
 
-  r.GET("/register", auth.Create)
-  r.POST("/register", auth.Register)
+	r.GET("/register", auth.Create)
+	r.POST("/register", auth.Register)
 
-  r.GET("/login", auth.Index)
-  r.POST("/login", auth.Login)
+	r.GET("/login", auth.Index)
+	r.POST("/login", auth.Login)
 
-  r.GET("/verified", auth.Verified)
+	r.GET("/verified", auth.Verified)
 
-  r.GET("/logout", auth.Logout)
-  r.Run()
+	r.GET("/logout", auth.Logout)
+
+	r.GET("/containers/", func(c *gin.Context) {
+		c.HTML(200, "containers/index.tmpl", gin.H{})
+	})
+	r.Run()
 }
