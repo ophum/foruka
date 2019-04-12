03e8fd9887f0214463eae5ffb85bafcee446b0ef
ophum <inagaki0106@gmail.com>
2019-04-12 13:53:32 +0900

    [update] create IsAuth method

--- controllers/authController/auth.go
+++ controllers/authController/auth.go
@@ -1,59 +1,69 @@
 package authController
 
 import (
-  "github.com/gin-gonic/gin"
-  "github.com/gin-gonic/contrib/sessions"
-  "github.com/ophum/foruka/models/authModel"
+	"github.com/gin-gonic/contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"github.com/ophum/foruka/models/authModel"
 )
 
+func IsAuth(c *gin.Context) bool {
+	session := sessions.Default(c)
+	id := session.Get("id")
+	if id != nil {
+		return true
+	}
+	return false
+}
+
 func Verified(c *gin.Context) {
-  session := sessions.Default(c)
-  id := session.Get("id")
-  if id != nil {
-    c.HTML(200, "verified.tmpl", gin.H{
-      "Id": id,
-    })
-  }else {
-    c.Redirect(301, "/login")
-  }
+
+	if IsAuth(c) {
+		session := sessions.Default(c)
+		id := session.Get("id")
+		c.HTML(200, "verified.tmpl", gin.H{
+			"Id": id,
+		})
+	} else {
+		c.Redirect(301, "/login")
+	}
 }
 func Index(c *gin.Context) {
-  c.HTML(200, "login.tmpl", gin.H{})
+	c.HTML(200, "login.tmpl", gin.H{})
 }
 
 func Login(c *gin.Context) {
-  id := c.PostForm("id")
-  pass := c.PostForm("pass")
+	id := c.PostForm("id")
+	pass := c.PostForm("pass")
 
-  v := authModel.Verify(id, pass)
+	v := authModel.Verify(id, pass)
 
-  if v {
-    session := sessions.Default(c)
-    session.Set("id", id)
-    session.Save()
-    c.Redirect(301, "/verified")
-  }else {
-    c.Redirect(301, "/login")
-  }
+	if v {
+		session := sessions.Default(c)
+		session.Set("id", id)
+		session.Save()
+		c.Redirect(301, "/verified")
+	} else {
+		c.Redirect(301, "/login")
+	}
 }
 
 func Create(c *gin.Context) {
-  c.HTML(200,"register.tmpl", gin.H{})
+	c.HTML(200, "register.tmpl", gin.H{})
 }
 
 func Register(c *gin.Context) {
-  id := c.PostForm("id")
-  pass := c.PostForm("pass")
+	id := c.PostForm("id")
+	pass := c.PostForm("pass")
 
-  authModel.Create(id, pass)
+	authModel.Create(id, pass)
 
-  c.Redirect(301, "/login")
+	c.Redirect(301, "/login")
 }
 
 func Logout(c *gin.Context) {
-  session := sessions.Default(c)
-  session.Clear()
-  session.Save()
-  
-  c.Redirect(301, "/")
+	session := sessions.Default(c)
+	session.Clear()
+	session.Save()
+
+	c.Redirect(301, "/")
 }
