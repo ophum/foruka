1adc120de2ac3e3bf1208e084831b67b9acfc450
ophum <inagaki0106@gmail.com>
2019-04-23 12:28:03 +0900

    [add] add Create action

--- controllers/containerController/container.go
+++ controllers/containerController/container.go
@@ -3,10 +3,27 @@ package containerController
 import (
 	"github.com/gin-gonic/gin"
 	auth "github.com/ophum/foruka/controllers/authController"
+	contmodel "github.com/ophum/foruka/models/containerModel"
 )
 
 func Index(c *gin.Context) {
-  auth.Auth(c)
+	auth.Auth(c)
 
-  c.HTML(200, "containers/index.tmpl", gin.H{"Title": "foruka", "C": "hoge"})
+	c.HTML(200, "containers/index.tmpl", gin.H{"Title": "foruka", "C": "hoge"})
+}
+
+func Create(c *gin.Context) {
+	auth.Auth(c)
+
+	c.HTML(200, "containers/create.tmpl", gin.H{})
+}
+
+func Store(c *gin.Context) {
+	auth.Auth(c)
+	userId := 1
+	name := c.PostForm("name")
+	image := c.PostForm("image")
+
+	contmodel.Create(userId, name, image)
+	c.Redirect(301, "/containers/")
 }
