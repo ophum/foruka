965d1d6c6561720db2d1f782be57cd069b04063c
hum_op <inagaki0106@gmail.com>
2019-04-25 19:06:58 +0900

    [add] add GetUser method and GetUserWhereName method

--- models/authModel/User.go
+++ models/authModel/User.go
@@ -1,42 +1,64 @@
 package authModel
 
 import (
-  "github.com/jinzhu/gorm"
-  _ "github.com/mattn/go-sqlite3"
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type User struct {
-  gorm.Model
-  Name string
-  Hash string
+	gorm.Model
+	Name string
+	Hash string
 }
 
+func GetUser(user_id uint) User {
+	db, err := gorm.Open("sqlite3", "database/database.sqlite")
+	if err != nil {
+		panic("failed to connect database\n")
+	}
+	defer db.Close()
 
-func Verify(id string, pass string) bool {
-  db, err := gorm.Open("sqlite3", "database/database.sqlite")
-  if err != nil {
-    panic("failed to connect database\n")
-  }
-  defer db.Close()
+	var user User
+	db.First(&user, user_id)
 
-  var user User
-  db.Where("name = ?", id).First(&user);
-
-  return CompareHash(user.Hash, pass)
+	return user
 }
 
-func Create(id string, pass string) error {
-  db, err := gorm.Open("sqlite3", "database/database.sqlite")
-  if err != nil {
-    panic("failed to connect database\n")
-  }
-  defer db.Close()
+func GetUserWhereName(name string) User {
+	db, err := gorm.Open("sqlite3", "database/database.sqlite")
+	if err != nil {
+		panic("failed to connect database\n")
+	}
+	defer db.Close()
 
-  hash, err := CreateHash(pass)
-  if err != nil {
-    return err
-  }
-  db.Create(&User{Name: id, Hash: hash})
-  return nil
+	var user User
+	db.Where("name = ?", name).First(&user)
+	return user
+}
+func Verify(name string, pass string) bool {
+	db, err := gorm.Open("sqlite3", "database/database.sqlite")
+	if err != nil {
+		panic("failed to connect database\n")
+	}
+	defer db.Close()
+
+	var user User
+	db.Where("name = ?", name).First(&user)
+
+	return CompareHash(user.Hash, pass)
 }
 
+func Create(name string, pass string) error {
+	db, err := gorm.Open("sqlite3", "database/database.sqlite")
+	if err != nil {
+		panic("failed to connect database\n")
+	}
+	defer db.Close()
+
+	hash, err := CreateHash(pass)
+	if err != nil {
+		return err
+	}
+	db.Create(&User{Name: name, Hash: hash})
+	return nil
+}
