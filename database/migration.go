5b2c04a2df5fffaa1aa3166a62b25f80ce663c9d
ophum <inagaki0106@gmail.com>
2019-05-07 09:50:34 +0900

    [change] change space indent to tab indent

--- database/migration.go
+++ database/migration.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-  "github.com/jinzhu/gorm"
-  _ "github.com/mattn/go-sqlite3"
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
 
-  auth "github.com/ophum/foruka/models/authModel"
+	auth "github.com/ophum/foruka/models/authModel"
 	cont "github.com/ophum/foruka/models/containerModel"
 )
 
 func main() {
-  db, err := gorm.Open("sqlite3", "database.sqlite")
-  if err != nil {
-    panic("failed to connect database\n")
-  }
-  defer db.Close()
-  
-  db.AutoMigrate(&auth.User{}, &cont.Container{})
-}
+	db, err := gorm.Open("sqlite3", "database.sqlite")
+	if err != nil {
+		panic("failed to connect database\n")
+	}
+	defer db.Close()
 
+	db.AutoMigrate(&auth.User{}, &cont.Container{})
+}
--- models/authModel/Hash.go
+++ models/authModel/Hash.go
@@ -1,25 +1,24 @@
 package authModel
 
 import (
-  "golang.org/x/crypto/bcrypt"
+	"golang.org/x/crypto/bcrypt"
 )
 
-func CreateHash(pass string) (string, error){
-  hash, err := bcrypt.GenerateFromPassword([]byte(pass),
-                                           bcrypt.DefaultCost)
+func CreateHash(pass string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(pass),
+		bcrypt.DefaultCost)
 
-  if err != nil {
-    return "", err
-  }
+	if err != nil {
+		return "", err
+	}
 
-  return string(hash), nil
+	return string(hash), nil
 }
 
 func CompareHash(hash string, pass string) bool {
-  err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(pass))
-  if err != nil {
-    return false
-  }
-  return true
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(pass))
+	if err != nil {
+		return false
+	}
+	return true
 }
-
