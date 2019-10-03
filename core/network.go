b4b0666b4b094413f4f4c41e1a16d2633c932e47
ophum <inagaki0106@gmail.com>
2019-10-03 15:16:18 +0900

    [add] 操作時にdatastoreに保存するようにした

--- core/network.go
+++ core/network.go
@@ -1,15 +1,23 @@
 package core
 
 import (
+	"encoding/json"
 	"fmt"
+
 	//lxd "github.com/lxc/lxd/client"
 	"github.com/lxc/lxd/shared/api"
+	"github.com/rs/xid"
 )
 
-func (f *Foruka) CreateNetwork(name string) error {
+type Network struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func (f *Foruka) CreateNetwork(name string) (string, error) {
 
 	if name == "" {
-		return fmt.Errorf("Error: Invalid Arguments, Empty `name`\n")
+		return "", fmt.Errorf("Error: Invalid Arguments, Empty `name`\n")
 	}
 
 	req := api.NetworksPost{
@@ -26,15 +34,52 @@ func (f *Foruka) CreateNetwork(name string) error {
 
 	err := f.server.CreateNetwork(req)
 	if err != nil {
+		return "", err
+	}
+
+	id := xid.New()
+	network := Network{
+		Id:   id.String(),
+		Name: name,
+	}
+	s, _ := json.Marshal(network)
+	f.ds.Set(fmt.Sprintf("networks/bridge/%s", id.String()), string(s))
+	return id.String(), nil
+}
+
+func (f *Foruka) DeleteNetwork(id string) error {
+	if id == "" {
+		return fmt.Errorf("Error: Invalid Arguments, Empty `name`\n")
+	}
+
+	s := f.ds.Get(fmt.Sprintf("networks/bridge/%s", id))
+	nw := &Network{}
+	_ = json.Unmarshal([]byte(s), &nw)
+	name := nw.Name
+	err := f.server.DeleteNetwork(name)
+	if err != nil {
 		return err
 	}
+	f.ds.Del(fmt.Sprintf("networks/bridge/%s", nw.Id))
 	return nil
 }
 
-func (f *Foruka) DeleteNetwork(name string) error {
-	if name == "" {
-		return fmt.Errorf("Error: Invalid Arguments, Empty `name`\n")
+func (f *Foruka) GetNetwork(id string) Network {
+	s := f.ds.Get(fmt.Sprintf("networks/bridge/%s", id))
+	fmt.Printf("GETNETWORK-->\n%s\n", s)
+	var network Network
+	_ = json.Unmarshal([]byte(s), &network)
+	fmt.Println(network)
+	return network
+}
+
+func (f *Foruka) GetNetworks() map[string]Network {
+	nws := f.ds.Gets("networks/bridge/%")
+	ns := map[string]Network{}
+	for _, v := range nws {
+		n := &Network{}
+		_ = json.Unmarshal([]byte(v), n)
+		ns[n.Id] = *n
 	}
-	err := f.server.DeleteNetwork(name)
-	return err
+	return ns
 }
