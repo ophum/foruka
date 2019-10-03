c64863ae8b06875ed3893536779de7c14d3bc61b
ophum <inagaki0106@gmail.com>
2019-10-03 16:04:01 +0900

    [update] datastoreで保存するようにした

--- core/container.go
+++ core/container.go
@@ -1,15 +1,25 @@
 package core
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"time"
+
+	"github.com/rs/xid"
 
 	lxd "github.com/lxc/lxd/client"
 	"github.com/lxc/lxd/shared/api"
 )
 
-func (f *Foruka) CreateContainer(name, alias string, iface map[string]string, limits map[string]string) error {
+type Container struct {
+	Id     string            `json:"id"`
+	Name   string            `json:"name"`
+	Iface  map[string]string `json:"iface"`
+	Limits map[string]string `json:"limits"`
+}
+
+func (f *Foruka) CreateContainer(name, alias string, iface map[string]string, limits map[string]string) (string, error) {
 	devices := map[string]map[string]string{}
 	for k, v := range iface {
 		devices[k] = map[string]string{
@@ -46,35 +56,58 @@ func (f *Foruka) CreateContainer(name, alias string, iface map[string]string, li
 
 	op, err := f.server.CreateContainer(req)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	err = op.Wait()
 	if err != nil {
-		return err
+		return "", err
 	}
-	return nil
+
+	id := xid.New()
+	c := &Container{
+		Id:     id.String(),
+		Name:   name,
+		Iface:  iface,
+		Limits: limits,
+	}
+
+	s, _ := json.Marshal(c)
+	f.ds.Set(fmt.Sprintf("container/%s", id.String()), string(s))
+	f.ds.Set(fmt.Sprintf("container/status/%s", id.String()), "stopped")
+	return id.String(), nil
 }
 
-func (f *Foruka) StartContainer(name string) error {
-	err := f.updateContainerState(name, "start")
+func (f *Foruka) GetContainerStatus(id string) string {
+	s := f.ds.Get(fmt.Sprintf("container/status/%s", id))
+	return s
+}
+
+func (f *Foruka) StartContainer(id string) error {
+	err := f.updateContainerState(id, "start")
 	if err != nil {
 		return err
 	}
+	f.ds.Set(fmt.Sprintf("container/status/%s", id), "running")
 	time.Sleep(time.Second)
 	return nil
 }
 
-func (f *Foruka) StopContainer(name string) error {
-	return f.updateContainerState(name, "stop")
+func (f *Foruka) StopContainer(id string) error {
+	f.ds.Set(fmt.Sprintf("container/status/%s", id), "stopped")
+	return f.updateContainerState(id, "stop")
 }
 
-func (f *Foruka) updateContainerState(name, action string) error {
+func (f *Foruka) updateContainerState(id, action string) error {
 
 	if action != "start" && action != "stop" && action != "freeze" && action != "unfreeze" {
 		return fmt.Errorf("Error: Invalid Arguments, undefined action `%s`\n", action)
 	}
 
+	s := f.ds.Get(fmt.Sprintf("container/%s", id))
+	con := &Container{}
+	_ = json.Unmarshal([]byte(s), &con)
+	name := con.Name
 	req := api.ContainerStatePut{
 		Action:  action,
 		Timeout: -1,
@@ -92,7 +125,12 @@ func (f *Foruka) updateContainerState(name, action string) error {
 	return nil
 }
 
-func (f *Foruka) ExecContainer(name string, command []string) error {
+func (f *Foruka) ExecContainer(id string, command []string) error {
+	s := f.ds.Get(fmt.Sprintf("container/%s", id))
+	con := &Container{}
+	_ = json.Unmarshal([]byte(s), &con)
+	name := con.Name
+
 	req := api.ContainerExecPost{
 		Command:     command,
 		WaitForWS:   true,
--- core/router.go
+++ core/router.go
@@ -1,32 +1,69 @@
 package core
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
+
+	"github.com/rs/xid"
 )
 
-func (f *Foruka) CreateRouter(name string, iface map[string]string) error {
+type Router struct {
+	Id    string            `json:"id"`
+	Cid   string            `json:"cid"`
+	Name  string            `json:"name"`
+	Iface map[string]string `json:"iface"`
+}
+
+func (f *Foruka) CreateRouter(name string, iface map[string]string) (string, error) {
 	if name == "" {
-		return fmt.Errorf("Error: Invalid Arguments, Empty `name`\n")
+		return "", fmt.Errorf("Error: Invalid Arguments, Empty `name`\n")
 	}
 
-	err := f.CreateContainer(name, "router", iface, map[string]string{
+	cid, err := f.CreateContainer(name, "router", iface, map[string]string{
 		"cpu":    "1",
 		"memory": "32MB",
 	})
 
 	if err != nil {
-		return err
+		return "", err
 	}
-	return nil
+
+	id := xid.New()
+	c := &Router{
+		Id:    id.String(),
+		Cid:   cid,
+		Name:  name,
+		Iface: iface,
+	}
+
+	s, _ := json.Marshal(c)
+	f.ds.Set(fmt.Sprintf("router/%s", id.String()), string(s))
+	return id.String(), nil
 }
 
-func (f *Foruka) StartRouter(name string) error {
-	return f.StartContainer(name)
+func (f *Foruka) StartRouter(id string) error {
+	s := f.ds.Get(fmt.Sprintf("router/%s", id))
+	r := &Router{}
+	_ = json.Unmarshal([]byte(s), &r)
+
+	return f.StartContainer(r.Cid)
 }
 
-func (f *Foruka) StopRouter(name string) error {
-	return f.StopContainer(name)
+func (f *Foruka) StopRouter(id string) error {
+	s := f.ds.Get(fmt.Sprintf("router/%s", id))
+	r := &Router{}
+	_ = json.Unmarshal([]byte(s), &r)
+
+	return f.StopContainer(r.Cid)
+}
+
+func (f *Foruka) GetRouterStatus(id string) string {
+	s := f.ds.Get(fmt.Sprintf("router/%s", id))
+	r := &Router{}
+	_ = json.Unmarshal([]byte(s), &r)
+
+	return f.GetContainerStatus(r.Cid)
 }
 
 type RouterInterface struct {
